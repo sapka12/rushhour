83f01feefa70b62de85fb8f43795c8e3a3422612
Arnold Farkas <farkasarnold1981@gmail.com>
2017-11-20 21:08:58 +0100

    GameSolver

---
+++ src/main/scala/hu/arnoldfarkas/rushhour/common/GameSolver.scala
@@ -0,0 +1,54 @@
+package hu.arnoldfarkas.rushhour.common
+
+trait GameSolver[S, M] {
+
+  val moves: Set[M]
+
+  def step(gameState: S, move: M): Option[S]
+
+  def isFinal(gameState: S): Boolean
+
+  type Path = List[M]
+  type History = (S, Path)
+  type HistoryLayer = Set[History] // Set[(S, List[M])]
+
+  private def nextLayer(historyLayer: HistoryLayer, visited: Set[S]): (HistoryLayer, Set[S]) = {
+
+    val layer =
+      for {
+        (state, path) <- historyLayer
+        (nextState, move) <- moves.map(m => (step(state, m), m))
+        if nextState.isDefined
+        if !visited.contains(state)
+      } yield (nextState.get, move :: path)
+
+    val allVisited = visited ++ layer.map{
+      case (nextState, _) => nextState
+    }
+
+    (layer, allVisited)
+  }
+
+  private def tree[A](historyLayer: HistoryLayer, visited: Set[S]): Stream[HistoryLayer] =
+    if (historyLayer isEmpty) Stream.empty[HistoryLayer]
+    else {
+      val (nextHistoryLayer, nextStates) = nextLayer(historyLayer, visited)
+      historyLayer #:: tree(nextHistoryLayer, visited ++ nextStates)
+    }
+
+  private def build(startState: S): Stream[History] =
+    tree(
+      Set((startState, List.empty[M])),
+      Set(startState)
+    ).flatten
+
+  def solve(startState: S): Option[Path] =
+    solution(build(startState)).headOption
+
+  def solution(histories: Stream[History]): Stream[Path] =
+    for {
+      (state, path) <- histories
+      if isFinal(state)
+    } yield path
+
+}
