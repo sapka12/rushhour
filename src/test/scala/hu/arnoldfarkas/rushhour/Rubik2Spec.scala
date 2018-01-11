cd06f62b65a15ac0df615e5c8059d13fde4005b6
Arnold Farkas <farkasarnold1981@gmail.com>
2018-01-11 21:14:28 +0100

    tests for rubik 2x2x2

--- src/test/scala/hu/arnoldfarkas/rushhour/Rubik2Spec.scala
+++ src/test/scala/hu/arnoldfarkas/rushhour/Rubik2Spec.scala
@@ -1,6 +1,6 @@
 package hu.arnoldfarkas.rushhour
 
-import hu.arnoldfarkas.rushhour.rubik.Rubik2State
+import hu.arnoldfarkas.rushhour.rubik.{Rubik2Solver, Rubik2State}
 import org.scalatest.{FlatSpec, Matchers}
 
 class Rubik2Spec  extends FlatSpec with Matchers {
@@ -22,4 +22,80 @@ class Rubik2Spec  extends FlatSpec with Matchers {
 
     set shouldNot contain(s3)
   }
+
+  it should "solve in 1 step" in {
+    val s1 = Rubik2State("brbrobobwwwwgrgryyyygogo".toCharArray.toSeq)
+
+    val solver = new Rubik2Solver()
+    val solution = solver.solve(s1)
+
+    val s = solution.get
+    s.size shouldBe 1
+
+  }
+
+  it should "solve in 2 step" in {
+    val s1 = Rubik2State("yrybgyoyobobwbwrrrggwgwo".toCharArray.toSeq)
+
+    val solver = new Rubik2Solver()
+    val solution = solver.solve(s1)
+
+    val s = solution.get
+    s.size shouldBe 2
+    println(s)
+  }
+
+  it should "solve in 5 step" in {
+
+    val start = System.currentTimeMillis
+
+    val s1 = Rubik2State("yrgorybogywrbowgbywowgrb".toCharArray.toSeq)
+
+    val solver = new Rubik2Solver()
+    val solution = solver.solve(s1)
+
+    val s = solution.get
+    s.size shouldBe 5
+    println(s)
+
+    val end = System.currentTimeMillis
+    println(end - start)
+  }
+
+
+  it should "solve in 8 step" in {
+
+    val start = System.currentTimeMillis
+
+    val s1 = Rubik2State("byogwygyrbowrwoorgyrwgbb".toCharArray.toSeq)
+
+    val solver = new Rubik2Solver()
+    val solution = solver.solve(s1)
+
+    val s = solution.get
+    s.size shouldBe 8
+    println(s)
+
+    val end = System.currentTimeMillis
+    println(end - start)
+  }
+
+
+//  it should "solve in 10 step" in {
+//
+//    val start = System.currentTimeMillis
+//
+//    val s1 = Rubik2State("gbyybgbrorgwyyowrogwrwob".toCharArray.toSeq)
+//
+//    val solver = new Rubik2Solver()
+//    val solution = solver.solve(s1)
+//
+//    val s = solution.get
+//    s.size shouldBe 10
+//    println(s)
+//
+//    val end = System.currentTimeMillis
+//    println(end - start)
+//  }
+
 }
